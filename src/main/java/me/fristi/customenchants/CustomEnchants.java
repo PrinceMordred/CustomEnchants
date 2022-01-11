1671204a9126334b0f5f76284129e42ccc0f9a04
PrinceMordred <jackrosenberg03@gmail.com>
2022-01-11 11:01:05 +0100

    innit

---
+++ src/main/java/me/fristi/customenchants/CustomEnchants.java
@@ -0,0 +1,17 @@
+package me.fristi.customenchants;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CustomEnchants extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
