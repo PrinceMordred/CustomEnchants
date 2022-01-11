ab99ebf3e52d13e5e6cac3032285e64000a594f8
PrinceMordred <jackrosenberg03@gmail.com>
2022-01-11 18:17:42 +0100

    Enchant? ion know what de fucjk goin on

--- src/main/java/me/fristi/customenchants/listeners/JoinListener.java
+++ src/main/java/me/fristi/customenchants/listeners/JoinListener.java
@@ -2,10 +2,14 @@ package me.fristi.customenchants.listeners;
 import me.fristi.customenchants.CustomEnchants;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.enchantments.EnchantmentOffer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.enchantment.PrepareItemEnchantEvent;
+import org.bukkit.event.player.PlayerGameModeChangeEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
@@ -36,8 +40,19 @@ public class JoinListener implements Listener {
         rod.addUnsafeEnchantment(CustomEnchants.grappling_hook_fishing_rod, 1);
         rod.setItemMeta(meta);
         player.getInventory().addItem(rod);
-        player.sendMessage(ChatColor.RED + "LMAO");
-        System.out.println(ChatColor.RED +"LMAOOOOOOOOO");
+    }
+    @EventHandler
+    public void onPlayerEnchant(PrepareItemEnchantEvent e){
+        e.getEnchanter().sendMessage(ChatColor.MAGIC+ "HMM, enchanting are we now");
+        EnchantmentOffer offer = new EnchantmentOffer(CustomEnchants.hemorrhage_axe, 1,1);
+        EnchantmentOffer offer2 = new EnchantmentOffer(CustomEnchants.hemorrhage_axe, 2,2);
+        EnchantmentOffer offer3 = new EnchantmentOffer(CustomEnchants.hemorrhage_axe, 3,3);
+        EnchantmentOffer[] offers = new EnchantmentOffer[3];
+        offers[0] = offer;
+        offers[1] = offer2;
+        offers[2] = offer3;
+        e = new PrepareItemEnchantEvent(e.getEnchanter(), (InventoryView) e.getInventory(), e.getEnchantBlock(), e.getItem(),offers, e.getEnchantmentBonus());
+
     }
 }
 
