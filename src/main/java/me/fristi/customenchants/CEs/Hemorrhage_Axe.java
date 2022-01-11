5234f6531550508c65d93e7d103a134d27688478
PrinceMordred <jackrosenberg03@gmail.com>
2022-01-11 11:26:43 +0100

    hemorrhage enchant

---
+++ src/main/java/me/fristi/customenchants/CEs/Hemorrhage_Axe.java
@@ -0,0 +1,48 @@
+package me.fristi.customenchants.CEs;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class Hemorrhage_Axe extends Enchantment {
+
+    @Override
+    public String getName() {
+        return "Hemorrhage";
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 5;
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.WEAPON;
+    }
+
+    @Override
+    public boolean isTreasure() {
+        return false;
+    }
+
+    @Override
+    public boolean isCursed() {
+        return false;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        return true;
+    }
+}
